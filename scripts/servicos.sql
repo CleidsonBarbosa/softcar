6726eb756a7d1a072b487178fff0d1b158f02482
Cleidson Barbosa <cledison-barbosa@hotmail.com>
2026-07-22 21:25:48 -0300

    script servicos sql

---
+++ scripts/servicos.sql
@@ -0,0 +1,11 @@
+INSERT INTO servicos (nome_servico, estoque_id_produto) VALUES
+('Lavagem simples externa', 41),
+('Lavagem completa interna + externa', 41),
+('Lavagem de motor', 44),
+('Cristalização de pintura', 42),
+('Vitrificação de pintura', 42),
+('Higienização interna', 55),
+('Revitalização de bancos de couro', 54),
+('Revitalização de pneus', 54),
+('Lavagem de estofados', 55),
+('Enceramento profissional', 43);
